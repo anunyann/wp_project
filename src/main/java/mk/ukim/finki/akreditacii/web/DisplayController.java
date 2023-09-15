f87cff729d4dbc9e7b9b3e2372542cc82436e22a
Џотов Борис <boris.dzhotov@students.finki.ukim.mk>
2023-09-15 16:16:46 +0000

    Update 2 files
    - /src/main/java/mk/ukim/finki/akreditacii/web/DisplayController.java
    - /src/main/resources/templates/study_program_edit.html

--- src/main/java/mk/ukim/finki/akreditacii/web/DisplayController.java
+++ src/main/java/mk/ukim/finki/akreditacii/web/DisplayController.java
@@ -2,17 +2,21 @@ package mk.ukim.finki.akreditacii.web;
 
 import mk.ukim.finki.akreditacii.model.StudyCycle;
 import mk.ukim.finki.akreditacii.model.professor.Professor;
+import mk.ukim.finki.akreditacii.model.study_program.StudyProgram;
 import mk.ukim.finki.akreditacii.model.study_program.StudyProgramDetails;
 import mk.ukim.finki.akreditacii.model.subject.Book;
 import mk.ukim.finki.akreditacii.model.subject.StudyProgramSubject;
 import mk.ukim.finki.akreditacii.model.subject.SubjectDetails;
+import mk.ukim.finki.akreditacii.repository.StudyProgramSubjectRepository;
 import mk.ukim.finki.akreditacii.service.DisplayService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -24,10 +28,12 @@ import static java.util.stream.Collectors.groupingBy;
 public class DisplayController {
 
     private final DisplayService service;
+    private final StudyProgramSubjectRepository programSubjectRepository;
 
 
-    public DisplayController(DisplayService service) {
+    public DisplayController(DisplayService service, StudyProgramSubjectRepository programSubjectRepository) {
         this.service = service;
+        this.programSubjectRepository = programSubjectRepository;
     }
 
     @GetMapping(value = {"/{accreditation}/{cycle}"})
@@ -93,7 +99,7 @@ public class DisplayController {
         }
     }
 
-    @PostMapping("/program/{program}/edit-subjects")
+    @GetMapping("/program/{program}/edit-subjects")
     public String programSubjectManagement(@PathVariable String program, @RequestParam(defaultValue = "mk") String lang, Model model){
 
         List<StudyProgramSubject> subjects = service.getProgramSubjects(program);
@@ -103,7 +109,6 @@ public class DisplayController {
 
         Map<StudyProgramSubject,List<StudyProgram>> subjectStudyProgramMap = new HashMap<StudyProgramSubject,List<StudyProgram>>();
 
-
         StudyProgramDetails studyProgramDetails = this.service.getStudyProgramDetailsById(program);
         if (!subjects.isEmpty()) {
             model.addAttribute("studyProgram", studyProgramDetails);
@@ -112,6 +117,37 @@ public class DisplayController {
         return "study_program_edit";
     }
 
+    //TODO: action for saving an edited subject
+    @PostMapping("/program/{program}/edit/{code}")
+    public String programEditSubject(@PathVariable String program,
+                                     @PathVariable String code,
+                                     @RequestParam Float credits,
+                                     @RequestParam Short semester,
+                                     @RequestParam StudyProgram studyProgram,
+                                     @RequestParam String subjectGroup){
+
+        return "redirect:study_program_edit";
+    }
+
+    //TODO: action for adding a subject to semester
+//    @PostMapping("/program/{program}/add")
+//    public String programAddSubject(@PathVariable String program, @RequestParam String code, @RequestParam Short semester, @RequestParam Boolean type){
+//        List<StudyProgramSubject> subjects = service.getProgramSubjects(program);
+//
+//        StudyProgramSubject temp = subjects.stream().filter(i->i.getId().equals(code)).findFirst().get();
+//
+//
+//        return "redirect:/study_program_edit";
+//    }
+
+    // Action for deleting a subject from semester (table row)
+    @PostMapping("/program/{program}/delete/{code}")
+    public String programSubjectDelete (@PathVariable String program, @PathVariable String code){
+        List<StudyProgramSubject> subjects = service.getProgramSubjects(program);
+
+        subjects.remove(subjects.stream().filter(i->i.getId().equals(code)).findFirst().get());
+        return "redirect:/study_program_edit";
+    }
 
     @GetMapping("/subject/{subjectId}")
     public String subjectDetails(@PathVariable String subjectId, Model model) {
