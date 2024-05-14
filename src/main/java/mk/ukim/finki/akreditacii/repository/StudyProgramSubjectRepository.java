9717634330efcd0648a855f94ed46420d68ac1eb
milosevska0 <79375858+milosevska0@users.noreply.github.com>
2024-05-14 11:45:07 +0200

    Remove unused imports

--- src/main/java/mk/ukim/finki/akreditacii/repository/StudyProgramSubjectRepository.java
+++ src/main/java/mk/ukim/finki/akreditacii/repository/StudyProgramSubjectRepository.java
@@ -2,10 +2,7 @@ package mk.ukim.finki.akreditacii.repository;
 
 import mk.ukim.finki.akreditacii.model.subject.StudyProgramSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface StudyProgramSubjectRepository extends JpaRepository<StudyProgramSubject, String> {
 
--- src/main/java/mk/ukim/finki/akreditacii/repository/SubjectRepository.java
+++ src/main/java/mk/ukim/finki/akreditacii/repository/SubjectRepository.java
@@ -3,7 +3,5 @@ package mk.ukim.finki.akreditacii.repository;
 import mk.ukim.finki.akreditacii.model.subject.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface SubjectRepository extends JpaRepository<Subject, String> {
 }
