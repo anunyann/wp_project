6caa7120ae29edbc0b72b06668864f8db80c5a04
milosevska0 <79375858+milosevska0@users.noreply.github.com>
2024-06-01 12:49:54 +0200

    Remove unused imports

--- src/main/java/mk/ukim/finki/akreditacii/service/impl/StudyProgramSubjectServiceImpl.java
+++ src/main/java/mk/ukim/finki/akreditacii/service/impl/StudyProgramSubjectServiceImpl.java
@@ -1,5 +1,8 @@
 package mk.ukim.finki.akreditacii.service.impl;
 
+import mk.ukim.finki.akreditacii.model.exceptions.InvalidStudyProgram;
+import mk.ukim.finki.akreditacii.model.exceptions.InvalidStudyProgramSubjectId;
+import mk.ukim.finki.akreditacii.model.exceptions.InvalidSubjectId;
 import mk.ukim.finki.akreditacii.model.study_program.StudyProgramDetails;
 import mk.ukim.finki.akreditacii.model.subject.StudyProgramSubject;
 import mk.ukim.finki.akreditacii.model.subject.SubjectDetails;
@@ -34,8 +37,7 @@ public class StudyProgramSubjectServiceImpl implements StudyProgramSubjectServic
 
     @Override
     public StudyProgramSubject findById(String subjectId) {
-        System.out.println("Subject id " + subjectId);
-        return studyProgramSubjectRepository.findAllBySubjectId(subjectId).stream().findFirst().get();
+        return studyProgramSubjectRepository.findById(subjectId).orElseThrow(() -> new InvalidStudyProgramSubjectId(subjectId));
     }
 
     @Override
@@ -45,7 +47,8 @@ public class StudyProgramSubjectServiceImpl implements StudyProgramSubjectServic
 
     @Override
     public StudyProgramSubject edit(String studyProgramSubjectId, String name, Boolean mandatory, short semester) {
-        StudyProgramSubject subject = studyProgramSubjectRepository.findById(studyProgramSubjectId).orElseThrow(RuntimeException::new);
+        StudyProgramSubject subject = findById(studyProgramSubjectId);
+
 
         subject.getSubject().getSubject().setName(name);
         subject.setMandatory(mandatory);
@@ -63,15 +66,14 @@ public class StudyProgramSubjectServiceImpl implements StudyProgramSubjectServic
     @Override
     public void add(String id, String subjectId, boolean mandatory, short semester, float order) {
         StudyProgramSubject studyProgramSubject = new StudyProgramSubject();
-        studyProgramSubject.setId(subjectId);
 
         String studyProgramSubjectId = id + "-" + subjectId;
         studyProgramSubject.setId(studyProgramSubjectId);
 
-        SubjectDetails subjectDetails = subjectDetailsRepository.findById(subjectId).get();
+        SubjectDetails subjectDetails = subjectDetailsRepository.findById(subjectId).orElseThrow(()-> new InvalidSubjectId(subjectId));
 
         studyProgramSubject.setSubject(subjectDetails);
-        studyProgramSubject.setStudyProgram(studyProgramService.findById(id).orElse(null));
+        studyProgramSubject.setStudyProgram(studyProgramService.findById(id).orElseThrow(() -> new InvalidStudyProgram(id)));
         studyProgramSubject.setMandatory(mandatory);
         studyProgramSubject.setSemester(semester);
         studyProgramSubject.setDependenciesOverride(null);
--- src/main/java/mk/ukim/finki/akreditacii/web/StudyProgramSubjectManagement.java
+++ src/main/java/mk/ukim/finki/akreditacii/web/StudyProgramSubjectManagement.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.akreditacii.web;
 
+import mk.ukim.finki.akreditacii.model.exceptions.InvalidStudyProgram;
 import mk.ukim.finki.akreditacii.model.study_program.StudyProgramDetails;
 import mk.ukim.finki.akreditacii.model.subject.StudyProgramSubject;
 import mk.ukim.finki.akreditacii.model.subject.Subject;
@@ -13,7 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
-@RequestMapping("admin/study-programs")
+@RequestMapping("admin/program")
 @Controller
 public class StudyProgramSubjectManagement {
 
@@ -29,7 +30,7 @@ public class StudyProgramSubjectManagement {
 
     @GetMapping("/{id}/edit-subjects")
     public String editSubjects(Model model, @PathVariable String id) {
-        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).get();
+        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).orElseThrow(() -> new InvalidStudyProgram(id));
         List<StudyProgramSubject> subjects = studyProgramSubjectService.findByStudyProgram(studyProgramDetails);
 
         model.addAttribute("studyProgramDetails", studyProgramDetails);
@@ -43,7 +44,7 @@ public class StudyProgramSubjectManagement {
         this.studyProgramSubjectService.edit(subjectId, name, mandatory, semester);
 
         redirectAttributes.addAttribute("id", id);
-        return "redirect:/admin/study-programs/" + id + "/edit-subjects";
+        return "redirect:/admin/program/{id}/edit-subjects".replace("{id}", id);
     }
 
     @GetMapping("/{id}/remove-subject/{subjectId}")
@@ -59,12 +60,12 @@ public class StudyProgramSubjectManagement {
         }catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", "An error occurred while removing the subject.");
         }
-        return "redirect:/admin/study-programs/" + id + "/edit-subjects";
+        return "redirect:/admin/program/{id}/edit-subjects".replace("{id}", id);
     }
 
     @GetMapping("{id}/add-subject")
     public String addSubjectForm(Model model, @PathVariable String id){
-        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).orElse(null);
+        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).orElseThrow(() -> new InvalidStudyProgram(id));
         List<Subject> subjects = subjectService.findAllSubjects();
 
         model.addAttribute("studyProgramDetails", studyProgramDetails);
@@ -75,16 +76,23 @@ public class StudyProgramSubjectManagement {
     }
 
     @PostMapping("{id}/add-subject")
-    public String addSubject(@PathVariable String id, @RequestParam String subjectId, @RequestParam boolean mandatory,  @RequestParam  short semester, @RequestParam float order, RedirectAttributes redirectAttributes) {
-        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).orElse(null);
-        if (studyProgramDetails != null) {
+    public String addSubject(@PathVariable String id, @RequestParam String subjectId, @RequestParam boolean mandatory,  @RequestParam  short semester, @RequestParam(required = false) Float order, RedirectAttributes redirectAttributes) {
+        StudyProgramDetails studyProgramDetails = studyProgramDetailsService.findById(id).orElseThrow(() -> new InvalidStudyProgram(id));
+
+        if (studyProgramDetails!=null){
+            if (order == null) {
+                redirectAttributes.addAttribute("id", id);
+                redirectAttributes.addFlashAttribute("error", "Order is required");
+                return "redirect:/admin/program/{id}/add-subject".replace("{id}", id);
+            }
 
             this.studyProgramSubjectService.add(id, subjectId, mandatory, semester, order);
-
             redirectAttributes.addAttribute("id", id);
-            return "redirect:/admin/study-programs/" + id + "/edit-subjects";
+
+            return "redirect:/admin/program/{id}/edit-subjects".replace("{id}", id);
+
         }
-        return "redirect:/admin/study-programs/" + id + "/edit-subjects";
+        return "redirect:/admin/program/{id}/edit-subjects".replace("{id}", id);
     }
 
 }
