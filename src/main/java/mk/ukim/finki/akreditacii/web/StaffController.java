f4a7249f8b07f651a938f02c9c182f2ab1416dd4
bojana.marojevikj <bojana@iwmnetwork.com>
2025-01-10 14:14:24 +0100

    made images smaller, removed card styles

--- src/main/java/mk/ukim/finki/akreditacii/web/StaffController.java
+++ src/main/java/mk/ukim/finki/akreditacii/web/StaffController.java
@@ -42,7 +42,7 @@ public class StaffController {
 
     @GetMapping
     public String listStaff(@RequestParam(defaultValue = "0") int page,
-                            @RequestParam(defaultValue = "12") int size,
+                            @RequestParam(defaultValue = "24") int size,
                             @RequestParam(required = false) UserRole role,
                             Model model) {
         Pageable pageable = PageRequest.of(page, size);
